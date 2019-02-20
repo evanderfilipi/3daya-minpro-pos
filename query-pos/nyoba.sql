71d46518eaf36c8195b16a8935f43d7beaa3b8b1
Muhammad Farhan <farhanitug@gmail.com>
2019-02-20 13:24:34 +0700

    ini table nyoba

---
+++ query-pos/nyoba.sql
@@ -0,0 +1,9 @@
+  [1/2]                             table                             Modified  
+
+create table nyoba(
+        id int,
+        nyoba_name character(64),
+        nyoba_desc character(64)
+);
+
+
